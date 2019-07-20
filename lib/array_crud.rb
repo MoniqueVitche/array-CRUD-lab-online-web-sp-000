7d9c711ca5491da7c9945f70c5e4e19592e06176
Monique Vitche <mvitche7@gmail.com>
2019-07-20 18:18:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,21 +1,21 @@
 def create_an_empty_array
-  create_an_empty_array[]
+  create_an_empty_array = []
 end
 
 def create_an_array
-  create_an_array[array]
+  create_an_array = [array]
 end
 
 def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_array.push[array1]
+  add_element_to_end_of_array.push = [array1]
 end
 
 def add_element_to_start_of_array(array, element)
-  add_element_to_start_of_array.upshift[array2]
+  add_element_to_start_of_array.upshift = [array2]
 end
 
 def remove_element_from_end_of_array(array)
-  remove_element_from_end_of_array.delete
+  
 end
 
 def remove_element_from_start_of_array(array)
